025b2c564bee5c73c544b424f984082a38fe9f1f
Apurva Jain <126224864+apurvajain4@users.noreply.github.com>
2024-07-07 20:11:16 +0530

    Delete Html.css.js,bootstarpLab directory

--- Html.css.js,bootstarpLab/HtmlLab4/HtmlLab4_Part2/script3.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('colorButton').addEventListener('click', function() {
-    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-    document.body.style.backgroundColor = randomColor;
-});
